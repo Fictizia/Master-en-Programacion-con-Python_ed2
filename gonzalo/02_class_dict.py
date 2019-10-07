b0f6870170382b8ef2e158c78954e60bbab9bfd6
gonzapython <gonza5152@gmail.com>
2019-10-07 21:49:40 +0200

    py de listas

---
+++ gonzalo/02_class_dict.py
@@ -0,0 +1,10 @@
+name = 'jose'
+frind_list = ['alberto', 'silvia', 'gerardo']
+
+my_dict = {'key': name,
+            'number': 30,
+            'friends': frind_list,
+            }
+
+print(my_dict)
+print(f'me llamo {my_dict["name"]}')
