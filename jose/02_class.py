2365662cfc665e38af8bd1f90785a4411edb5404
JMGallego <jmgallego@ZiMac4-Master.local>
2019-10-07 20:50:24 +0200

    examples

---
+++ jose/02_class.py
@@ -0,0 +1,37 @@
+#BASIC STRINGS
+
+name = 'Jose'
+age = 36
+print(f'hola {name} tienes {age} años,y sé que {name} realmente no es tu nombre')
+
+print('hola {nombre} tienes {edad} años,y sé que {nombre} realmente no es tu nombre'.format(nombre=name, edad=age))
+
+print('hola ' * 5)
+
+texto_largo = 'En un lugar de la mancha, de cuyo nombre no puedo acordarme'
+print(texto_largo[:24])
+
+# BASIC NUMBERS
+
+entero = 1
+decimales = 2.1
+otro_decimal = 2.0
+
+print(entero+entero)
+print(entero+decimales)
+print(entero+otro_decimal)
+print(round(entero+otro_decimal))
+
+# BASIC VARIABLES
+
+a = 1
+b = a
+a = 2
+print(b)
+
+c = 'hola'
+d = c
+c = 'Mundo'
+print(d)
+
+
---
+++ jose/02_class_complex.py
@@ -0,0 +1,15 @@
+# listas
+
+fruits = ['banaba','pair', 'apple']
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+fruits.append('pineapple')
+print(fruits[3])
+
+summer_fruits = ['melon', 'watermelon']
+more_fruits = summer_fruits
+print(more_fruits)
+more_fruits[1] = 'pair'
+print(more_fruits)
+print(summer_fruits)
