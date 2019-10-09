6ed531fdb981ffc8aeee7b7bd1bfc2fe359ae3cd
albertow14 <albertov1470@gmail.com>
2019-10-09 21:48:11 +0200

    clase

---
+++ alberto/02_class.py
@@ -0,0 +1,21 @@
+name = 'Alberto'
+age = 26
+texto_largo = 'En un lugar de la mancha, de cuyo nombre no quiero acordarme'
+#print('hola {nombre} tienes {edad} años, y se que {nombre} realmente no es tu nombre' .format(nombre=name, edad=age))
+#print ('hola' * 5)
+#print(texto_largo[:24])
+
+#Numero básicos
+entero = 1
+decimales = 2.5
+otro_decimal= 2.0
+
+print(entero+otro_decimal)
+
+#Basic variables
+a = 1
+b = a
+a = 8
+
+
+f
