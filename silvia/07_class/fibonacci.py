de7d969d5f26383e6e7e6eede80581732d49073b
Silvia <sarnaizgarcia@gmail.com>
2019-10-20 15:03:23 +0200

    Actualización Fibonacci

--- silvia/07_class/fibonacci.py
+++ silvia/07_class/fibonacci.py
@@ -1,20 +1,13 @@
 # La sucesión comienza con los números 0 y 1, 2
-# A partir de estos, «cada término es la suma de los dos anteriores»
-# 0 + 1 = 1 ----- n = 1 + (1-1)
-# 1 + 1 = 2
-# 1 + 2 = 3
-# 2 + 3 = 5
-# 3 + 5 = 8 ----- n = 8
-
-lista_fibonacci = list()
+# A partir de estos, cada término es la suma de los dos anteriores
 
 
-# def fibonacci(n):
-#     if n <= 1:
-#         return n
-#     else:
-#         return fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
-# result = fibonacci(9)
-# print(result)
+result = fibonacci(9)
+print(result)
