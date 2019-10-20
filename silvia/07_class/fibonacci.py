2ca7ac99aba477ed067d9bd72c70726969c72d44
Silvia <sarnaizgarcia@gmail.com>
2019-10-20 15:02:07 +0200

    Sucesión de Fibonacci con recursividad

---
+++ silvia/07_class/fibonacci.py
@@ -0,0 +1,20 @@
+# La sucesión comienza con los números 0 y 1, 2
+# A partir de estos, «cada término es la suma de los dos anteriores»
+# 0 + 1 = 1 ----- n = 1 + (1-1)
+# 1 + 1 = 2
+# 1 + 2 = 3
+# 2 + 3 = 5
+# 3 + 5 = 8 ----- n = 8
+
+lista_fibonacci = list()
+
+
+# def fibonacci(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+
+
+# result = fibonacci(9)
+# print(result)
