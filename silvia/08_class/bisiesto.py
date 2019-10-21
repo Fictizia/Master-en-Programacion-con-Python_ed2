f096d38d8c317ce159a35144ed68756fce25d3bf
sarnaizgarcia <sarnaizgarcia@gmail.com>
2019-10-21 21:45:03 +0200

    08 - bisiesto

--- silvia/08_class/bisiesto.py
+++ silvia/08_class/bisiesto.py
@@ -7,8 +7,20 @@ def is_leap(year):
         return True
     return False # Evita que tenga que validar el else; sólo valida el if.
 
-year = input_year()
-if is_leap(year):
-    print(f'{year} es bisiesto')
-else:
-    print(f'{year} no es bisisesto')
+def validate_year(year):
+    if year < 1582 or year > 2020:
+        print('Escribe un año entre 1582 y 2020')
+        return False
+    return True
+
+def main():
+    year = input_year()
+    if validate_year(year):
+        if is_leap(year):
+            print(f'{year} es bisiesto')
+        else:
+            print(f'{year} no es bisisesto')
+    else:
+        main()
+
+main()
