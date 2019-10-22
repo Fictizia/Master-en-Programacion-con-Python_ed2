2251759e2f085aa0d1b950e9d49b098b36b2e4a7
Silvia <sarnaizgarcia@gmail.com>
2019-10-22 15:24:46 +0200

    Actualización white jack que informa de carta

--- silvia/white_jack/white_jack_carta.py
+++ silvia/white_jack/white_jack_carta.py
@@ -1,31 +1,26 @@
-from silvia.white_jack.maze import give_card, english_deck 
+from maze import give_card, english_deck
 
-print(english_deck)
-card = give_card()
+# print(english_deck)
+# card = give_card(english_deck)
 
-print(card)
-print('ED', english_deck)
+# print(card)
+# print('ED', english_deck)
 
 
 def continue_game(english_deck, player_points, bank_points, player_game=True, bank_game=True):
     """
-    Parámetros: english_deck, player_points, bank_points
-
+    Parámetro 1: english_deck. Parámetro 2: player_points. Parámetro 3: bank_points
     Hay un input con el que el jugador indica si quiere recibir otra carta y le dice el número de puntos que tiene
-
     Si el jugador tiene más de 21 puntos, pierde
-
     La banca recibe carta siempre que tenga menos de 15 puntos. Si se pasa de 21, pierde
-
     Después de cada turno, evalúa el resultado
-
     return None
     """
 
     if player_game:
         question = input('¿Quieres carta? (sí o no): ')
         if 'S' in question.upper() and player_game:
-            player_card_value = give_a_card()
+            player_card_value = give_card(english_deck)
             print(f'Carta jugador {player_card_value}')
             player_points += player_card_value
             print(f'Tienes {player_points} puntos')
@@ -40,7 +35,7 @@ def continue_game(english_deck, player_points, bank_points, player_game=True, ba
     if bank_game:
 
         if bank_points < 15 and bank_game:
-            bank_card_value = give_a_card()
+            bank_card_value = give_card(english_deck)
             print(f'La banca recibe carta')
             bank_points += bank_card_value
             print(f'Total BANCA {bank_points}')
@@ -67,7 +62,7 @@ def evaluate_result(player_points, bank_points):
             f'Gana la banca. Tiene {bank_points} puntos y tú tienes {player_points} puntos')
 
 
-player_points = give_a_card()  # Contador de puntos del jugador
-bank_points = give_a_card()  # Contador de puntos de la banca
+player_points = give_card(english_deck)  # Contador de puntos del jugador
+bank_points = give_card(english_deck)  # Contador de puntos de la banca
 print(f'Tienes {player_points} puntos')
 continue_game(english_deck, player_points, bank_points)
