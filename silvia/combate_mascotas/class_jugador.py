66b6b92746ff5ebcd7917b80d01552af33babb50
Silvia <sarnaizgarcia@gmail.com>
2019-10-25 17:29:02 +0200

    Avance en combate de mascotas

--- silvia/combate_mascotas/class_jugador.py
+++ silvia/combate_mascotas/class_jugador.py
@@ -1,7 +1,10 @@
 class Jugador:
-    def __init__(self, mascota):
-        self.mascota = mascota
+    # def __init__(self, mascota):
+    #     self.mascota = mascota
 
-    def elegir_mascota(self, id_mascota):
-        mascota = int(input('Indica el número de la mascota que eliges: '))
+    def elegir_mascota(self, opciones):
+        id_mascota = int(input('Indica el número de la mascota que eliges: '))
+        mascota = opciones[id_mascota]
+        print(f'Has elegido a {mascota.nombre}')
+
         return mascota
--- silvia/combate_mascotas/class_mascotas.py
+++ silvia/combate_mascotas/class_mascotas.py
@@ -2,18 +2,25 @@ from random import randint
 
 
 class Mascota:
-    def __init__(self, ataque, defensa, vida):
+    def __init__(self, identificador, nombre, ataque, defensa, vida):
+        self.identificador = identificador
+        self.nombre = nombre
         self.ataque = ataque
         self.defensa = defensa
         self.vida = vida
 
+    def __repr__(self):
+        return f'{self.identificador} - {self.nombre}. Ataque: {self.ataque}. Defensa: {self.defensa}. Vida: {self.vida}'
+
     def atacar(self):
         ataque = randint(1, 5)
+        return ataque
 
     def defenderse(self):
-        pass
+        defensa = randint(1, 5)
+        return defensa
 
-    def derrotado(self):
+    def esta_vivo(self):
         if self.vida > 0:
             return False
         else:
